47f57bd17c67219306b55b982f8fe962aef632ee
p-bear <friendship41@cuk.edu>
2023-12-26 20:02:23 +0900

    [sample][0.0.0] Actuator 제외

--- pbear-spring/pbear-sample/src/main/java/com/pbear/sample/RestEcho.java
+++ pbear-spring/pbear-sample/src/main/java/com/pbear/sample/RestEcho.java
@@ -7,16 +7,31 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import java.util.Set;
+
 @Configuration
 @Slf4j
 public class RestEcho {
+  private static final Set<String> excludePathSet = Set.of(
+      "actuator"
+  );
+
   @Bean
   public RouterFunction<ServerResponse> echoRoute() {
     return RouterFunctions.route()
-        .route(request -> true, request -> {
-          log.info(" >> {}", request.path());
-          return ServerResponse.ok().build();
-        })
+        .route(
+            request -> {
+              for (String path : excludePathSet) {
+                if (request.path().startsWith(path)) {
+                  return false;
+                }
+              }
+              return true;
+            },
+            request -> {
+              log.info(" >> {}", request.path());
+              return ServerResponse.ok().build();
+            })
         .build();
   }
 
