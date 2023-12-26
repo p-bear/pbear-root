fbc4ac60e0a59f39d5bbe2f1e481205116a0e560
p-bear <friendship41@cuk.edu>
2023-12-26 23:46:54 +0900

    [lib][0.0.4] Actuator Properties external load 구현

--- pbear-spring/pbear-lib/src/main/java/com/pbear/lib/configurer/YamlConfigurer.java
+++ pbear-spring/pbear-lib/src/main/java/com/pbear/lib/configurer/YamlConfigurer.java
@@ -2,46 +2,35 @@ package com.pbear.lib.configurer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnResource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 
 @Configuration
 @Slf4j
 public class YamlConfigurer {
   @Bean
-  public YamlPropertiesFactoryBean yamlPropertiesFactoryBean(final ApplicationContext applicationContext) {
+  @ConditionalOnResource(resources = "classpath:application-secret.yml")
+  public YamlPropertiesFactoryBean yamlPropertiesFactoryBean() {
     YamlPropertiesFactoryBean bean = new YamlPropertiesFactoryBean();
-    List<Resource> resourceList = new ArrayList<>();
-    this.addClassPathResourceIfExsist(resourceList, "application-actuator.yml", applicationContext);
-    this.addClassPathResourceIfExsist(resourceList, "application-secret.yml", applicationContext);
-    bean.setResources(resourceList.toArray(new Resource[0]));
+    bean.setResources(new ClassPathResource("application-secret.yml"));
     return bean;
   }
 
   @Bean
+  @ConditionalOnBean(YamlPropertiesFactoryBean.class)
   public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(final YamlPropertiesFactoryBean yamlPropertiesFactoryBean) {
     PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
-    Properties properties = yamlPropertiesFactoryBean.getObject();
-    if (properties != null) {
-      configurer.setProperties(properties);
+    Properties secretProperties = yamlPropertiesFactoryBean.getObject();
+    if (secretProperties != null) {
+      log.info("apply application-secret.yml");
+      configurer.setProperties(secretProperties);
     }
     return configurer;
-  }
-
-  private void addClassPathResourceIfExsist(final List<Resource> resourceList, final String fileName, final ApplicationContext applicationContext) {
-    if (applicationContext.getResource("classpath:" + fileName).exists()) {
-      resourceList.add(new ClassPathResource(fileName));
-      log.info("ClassPath Resource Added: {}", fileName);
-    } else {
-      log.info("ClassPath Resource not exist, {}", fileName);
-    }
   }
 }
